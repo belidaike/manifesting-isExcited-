23e4e4721dca6f97fe958d9f47275268e45c48fc
unknown <ikebelida156@gmail.com>
2024-07-26 20:54:56 +0800

    updated

--- frontend/src/pages/Home/Aside.jsx
+++ frontend/src/pages/Home/Aside.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Context } from '../../context/Context'
 import { FaPeopleGroup } from "react-icons/fa6";
 import { LuTimerOff } from "react-icons/lu";
@@ -8,18 +8,29 @@ import { FaUserTimes } from "react-icons/fa";
 const Aside = () => {
     const { setCategory } = useContext(Context)
 
+    const [fourCat, setFourCat] = useState({
+        popular: true,
+        latest: false,
+        oldest: false,
+        leastP: false
+    })
+
     return (
         <aside>
             <ul>
-                <li onClick={() => setCategory('popular')}>
-                    <FaPeopleGroup /><span>Popular</span></li>
-                <li onClick={() => setCategory('latest')}>
-                    <MdOutlineAccessTime /> <span>Latest</span>
+                <li className={fourCat.popular === true ? 'underLineCSS' : "CSSDefault"} onClick={() => { setCategory('popular'), setFourCat({ ...fourCat, popular: true, latest: false, oldest: false, leastP: false }) }}>
+                    <FaPeopleGroup /><span >Popular</span>
                 </li>
-                <li onClick={() => setCategory('oldest')}>
+
+                <li className={fourCat.latest === true ? 'underLineCSS' : 'CSSDefault'} onClick={() => { setCategory('latest'), setFourCat({ ...fourCat, latest: true, popular: false, oldest: false, leastP: false }) }}>
+                    <MdOutlineAccessTime /> <span >Latest</span>
+                </li>
+
+                <li className={fourCat.oldest === true ? 'underLineCSS' : 'CSSDefault'} onClick={() => { setCategory('oldest'), setFourCat({ ...fourCat, oldest: true, latest: false, popular: false, leastP: false }) }}>
                     <LuTimerOff /><span>Oldest</span>
                 </li>
-                <li onClick={() => setCategory('leastPopular')}>
+
+                <li className={fourCat.leastP === true ? 'underLineCSS' : 'CSSDefault'} onClick={() => { setCategory('leastPopular'), setFourCat({ ...fourCat, leastP: true, latest: false, oldest: false, popular: false }) }}>
                     <FaUserTimes /><span>Least Popular</span>
                 </li>
             </ul>
--- frontend/src/pages/Home/Main.jsx
+++ frontend/src/pages/Home/Main.jsx
@@ -10,43 +10,18 @@ const Main = ({ similarMovies }) => {
 
     const displayMovies = similarMovies || sortedMovies[category]
     const displayMoviesCSS = similarMovies ? 'main-container-sim' : 'main-container'
-    const [popular, setPopular] = useState(true)
-    const [latest, setLatest] = useState(false)
-    const [oldest, setOldest] = useState(false)
-    const [leastP, setLeastP] = useState(false)
-    const [fourCat, setFourCat] = useState({
-        popular: true,
-        latest: false,
-        oldest: false,
-        leastP: false
-    })
+
+
     if (loading) {
         return <div>Loading...</div>
     }
-    // console.log(fourCat.map(cat => cat.popular))
     return (
         <main>
-            <button className={fourCat.popular === true ? 'popularCSS' : "popularCSSDefault"} onClick={() => setFourCat({ ...fourCat, popular: true, latest: false, oldest: false, leastP: false })}>popular</button>
-            <button className={fourCat.latest === true ? 'latestCSS' : 'latestCSSDefault'} onClick={() => setFourCat({ ...fourCat, latest: true, popular: false, oldest: false, leastP: false })}>latest</button>
-            <button className={fourCat.oldest === true ? 'oldestCSS' : 'oldestCSSDefault'} onClick={() => setFourCat({ ...fourCat, oldest: true, latest: false, popular: false, leastP: false })}>oldest</button>
-            <button className={fourCat.leastP === true ? 'oldestCSS' : 'oldestCSSDefault'} onClick={() => setFourCat({ ...fourCat, leastP: true, latest: false, oldest: false, popular: false, })}>least Popular</button>
-            {/* <button className={popular ? 'popularCSS' : 'popularCSSDefault'} onClick={() => {
-                setPopular(true), setLatest(false), setOldest(false), setLeastP(false)
-            }}>popular</button>
-            <button className={latest ? 'latestCSS' : 'latestCSSDefault'} onClick={() => {
-                setLatest(true), setPopular(false), setOldest(false), setLeastP(false)
-            }}>latest</button>
-            <button className={oldest ? 'oldestCSS' : 'oldestCSSDefault'} onClick={() => {
-                setOldest(true), setPopular(false), setLatest(false), setLeastP(false)
-            }}>oldest</button>
-            <button className={leastP ? 'leastPCSS' : 'leastPCSSDefault'} onClick={() => {
-                setLeastP(true), setPopular(false), setLatest(false), setOldest(false)
-            }}>leastP</button> */}
 
             <div className={displayMoviesCSS}>
                 {displayMovies.length > 0 ? (
                     displayMovies.map((movie) => (
-                        <div className='main-content' >
+                        <div className='main-content' key={movie.id}>
                             <div className="main-img-container">
                                 <img className='main-img' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" onClick={() => nav(`/movie/${movie.id}`)} key={movie.id} />
                             </div>
